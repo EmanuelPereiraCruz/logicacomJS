b4ffe025029002b4f606830b0ea86c4815822ceb
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-04-08 09:22:04 -0300

    Update cap02-Exem1.3.js
    - Uso do return;
    
    - Uso do textContent;
    
    - Uso do método .toFixed();
    
    - Referência a um botão HTML

--- Cap02/cap02-Exem1.3.js
+++ Cap02/cap02-Exem1.3.js
@@ -1,6 +1,8 @@
 function calculaPromocao(){
     let nomeDoCarro = document.getElementById("inVeiculo").value;
-    //Crie um IF pra verificar se o campo carro foi preenchido.
+    //Crie um IF pra verificar se o campo carro foi preenchido. Caso o campo esteja vazio, o alert é realizado.
+    /*O return no fim do código faz com que ele não prossiga com a execução das demais partes do código.
+    Caso não houvesse esse return, o código seria executado, mesmo que as informações não fossem preenchidas.*/
     if(nomeDoCarro == ""){
         alert("Por favor, preencha o nome do carro!");
         return;
@@ -16,13 +18,18 @@ function calculaPromocao(){
     }
 
     //Crie uma variável onde calculo 50% do preço do carro.
-    //Já a segunda variável, parcela, eu peguei o valor da entrada(50%) e depois dividi por 12.
     let entrada = precoDoCarro/2
+
+    //Já a segunda variável, parcela, eu peguei o valor da entrada(50%) e depois dividi por 12.
     let parcela = entrada/12
 
+    /*Aqui, uso o textContent para exibir informações ao usuário, nesse caso: nome do carro + valores pagos.
+    Além disso, usando o método .toFixed() consigo definir a quantidade de casas decimais que será exibida.*/
     outCarro.textContent = "Promoção: "+nomeDoCarro;
     outValores.textContent = "Entrada de R$: "+entrada.toFixed(2)+" + 12 parcelas de R$: "+parcela.toFixed(2);
 }
 
+/*Por fim, criei uma variável que faz referência ao botão do documento HTML.
+Quando o botão é clicado, a função calculaPromocao é realizada. Para que isso possa ocorrer, é fundamental usar o addEventListener*/
 let btPromocao = document.getElementById("btPromocao");
 btPromocao.addEventListener("click", calculaPromocao);
