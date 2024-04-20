bb46eedb54357dbc2647819c42cdb5db5c6ffbbf
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-04-20 17:03:03 -0300

    Update cap03-Exem3.5.js

--- Cap03/cap03-Exem3.5.js
+++ Cap03/cap03-Exem3.5.js
@@ -3,11 +3,17 @@ function calculaNotasDisponiveis(){
     let saque = document.getElementById("inSaque").value;
     saque = Number(saque);
 
+    /*Através desse if, verifico se o campo foi preenchido, ou seja, não está vazio.
+    Também verifico se o número é um múltiplo de 10, assim asseguro que o usuário informará um valor que seja divisivel por 10.*/
+    
     if(saque<10 || saque%10 != 0){
         alert("O valor minimo para SAQUE é de 10,00! Por favor, informe um valor igual ou maior que 10,00 R$");
         return;
     }
 
+    /*Aqui, faço a validação para notas de 10. Caso o valor do saque esteja entre 10 e 50, divido esse valor por 10 para obter 
+    a quantidade de notas de 10,00 disponíveis.*/
+    
     if(saque>=10 && saque<50){
         var qtdNotasDeDez = saque/10;
         qtdNotasDeDez = parseInt(qtdNotasDeDez);
@@ -15,6 +21,18 @@ function calculaNotasDisponiveis(){
         notasDe10.textContent = "Notas de R$ 10: "+qtdNotasDeDez;
     }
 
+    /*Nesse terceiro IF, faço uma validação para saques maiores que 50 porém, menores que 100.
+    Primeiro, eu divido o valor do saque por 50, assim obtenho a quantidade de notas de 50,00 disponíveis.
+    Após isso, usando o parseInt, obtenho apenas a parte inteira da divisão.
+    
+    Após isso, criei uma segunda variável chamada restoDeCinquenta, nela armazeno o valor do resto da divisão do saque por 50.
+    Ou seja, se o saque foi de 80,00 o resto da divisão é 30,00 que irá ficar armazenado na variável: restoDeCinquenta.
+    Após isso, pego esse valor e divido por 10 para obter apenas a quantidade de notas de 10,00.
+    
+    Por fim, uso outro if para verificar se a restoDeCinquenta tem valor diferente de 0. Assim, se o valor dela for 0, não será exibido nada.
+    Porém, se o valor dela for diferente de 0, exibirá o texto.
+    */
+    
     if(saque>=50 && saque<100){
         var qtdNotasDeCinquenta = saque/50;
         qtdNotasDeCinquenta = parseInt(qtdNotasDeCinquenta);
