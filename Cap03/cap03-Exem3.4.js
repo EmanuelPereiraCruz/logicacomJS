4c4c507a187f1c28df836d0502d79c7fec0bfaa5
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-04-20 16:39:23 -0300

    Update cap03-Exem3.4.js
    Observações sobre:
    
    - Método Math.sqrt;
    
    - Método Number.isInteger.

--- Cap03/cap03-Exem3.4.js
+++ Cap03/cap03-Exem3.4.js
@@ -2,13 +2,23 @@ function calculaRaiz(){
     let numero = document.getElementById("inNumero").value;
     numero = Number(numero);
 
+    //Este if valida o campo número, com isso estou buscando assegurar que ele seja preenchido corretamente.
     if(numero =="" || numero <0 || isNaN(numero)){
         alert("Por favor, informe um número válido!");
         return;
     }
 
+    /*Usando o método Math.sqrt, consigo calcular a raiz quadrada do número informado pelo usuário.
+    -Por exemplo, se o usuário informou o número = 25, a raiz quadrada dele é 5.*/
+    
     let raizQuadrada = Math.sqrt(numero);
 
+    /*Usando o método Number.isInteger, verifico se a raiz quadrada é ou não um número inteiro.
+    O resultado desse método é um boolean, portanto, ele vai me retornar verdadeiro ou falso.
+    Ainda a respeito do exemplo acima, se a raiz de 25 é 5, o método Number.isInteger irá me retornar um true.
+    Porém, se o número informado pelo usuário fosse 26, ele me retornaria um false, pois 26 não tem raiz exata.
+    */
+    
     if(Number.isInteger(raizQuadrada) == true ){
         outSaida.textContent = "A raiz quadrada do número informado é: "+raizQuadrada;
 
