b6351a9ac2a7fcedad49b338915bb60c97689f75
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-04-23 10:12:52 -0300

    Update cap03-Exem4.0-Extra.js
    Nesse exemplo, usei o else if. Adicionei 1 comentário exemplificando o funcionamento dele.

--- Cap03/cap03-Exem4.0-Extra.js
+++ Cap03/cap03-Exem4.0-Extra.js
@@ -2,6 +2,12 @@ function checaNumero(){
     let numero = document.getElementById("inNumero").value;
     numero = Number(numero);
 
+    /*Para o exemplo, usei o else if.
+    
+    1- Se o número é menor que zero, ele é negativo.
+    
+    2 - Se é maior que zero, ele é positivo.*/
+    
     if(numero<0){
         outSaida.textContent = "O número informado é negativo!"
     }else if(numero==0){
