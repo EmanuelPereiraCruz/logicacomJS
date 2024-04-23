5bdca4c6f448b948c07ed2850a4c97c14433dab4
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-04-23 10:16:01 -0300

    Update cap03-Exem4.1-Extra.js
    - Uso do operador ternário.
    
    Adicionei um comentário exemplificando seu uso.

--- Cap03/cap03-Exem4.1-Extra.js
+++ Cap03/cap03-Exem4.1-Extra.js
@@ -2,6 +2,14 @@ function verificaNumero(){
     let numero = document.getElementById("inNumero").value;
     numero = Number(numero);
 
+    /*
+    Nesse exemplo, usei o operador ternário! :D
+    
+    Através dele, podemos simplificar o uso do if.
+    Usando o operador ternário, na variável resultado armazenamos o valor da verificação.
+    Ou seja, se o resto da divisão de numero por 2 for igual a 0, ou seja, true, na variável resultado será armazenado "O número informado é par.
+    Caso contrário, será armazenado na variável a seguinte informação: "O número informado é ÍMPAR!"*/
+    
     let resultado = numero%2==0 ? outSaida.textContent = "O número informado é PAR!" : outSaida.textContent = "O número informado é ÍMPAR!";
 
 }
