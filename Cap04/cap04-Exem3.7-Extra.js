f62b52186ded4c2890741ea1eae6aaa7e2e9abd0
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-05-13 20:46:13 -0300

    Update cap04-Exem3.7-Extra.js
    No exemplo 3.7 adicionei a lógica do código e a utilização do "use strict" 
    
    - Utilizei o use strict para assegurar a existência das variáveis;
    
    - Usei If para verificar se o número é divisível por 7

--- Cap04/cap04-Exem3.7-Extra.js
+++ Cap04/cap04-Exem3.7-Extra.js
@@ -1,18 +1,22 @@
-"use strict"
+"use strict" //Utilizei o use strict para assegurar a existência das variáveis
+
 /*
 
 Contagem regressiva de 100 a 1, exibindo apenas os números divisíveis por 7:
-Escreva um programa que use um laço for para contar de 100 a 1 em ordem decrescente e exibir apenas os números divisíveis por 7.
+
+Escreva um programa que use um while para contar de 100 a 1 em ordem decrescente e exibir apenas os números divisíveis por 7.
 
 */
 
-//Usei while ao invés de laço for
-
+//Variável de inicialização - inicia com valor igual a 100
 let count = 100;
 
 console.log("|----Números divisíveis por 7 entre 100 e 1----|"+"\n")
 
+
 while(count>0){
+    //O if verifica se o número é divisível por 7. Caso seja verdadeiro, ele imprimi o número através do console.log
+    
     if(count%7==0){
         console.log(count)
     }
