4f54068b392a5a8a9e99b062a4552f2feb61395a
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-05-13 09:51:33 -0300

    Update cap04-Exem3.6-Extra.js
    No exemplo 3.6 adicionei observações a respeito da lógica implementada na resolução do exercício.
    
    Além do for, usei:
    - IF;
    - %;
    - !=

--- Cap04/cap04-Exem3.6-Extra.js
+++ Cap04/cap04-Exem3.6-Extra.js
@@ -5,6 +5,12 @@ Escreva um programa que use um laço for para imprimir os números de 1 a 20, ex
 
 */
 
+//A variável de inicialização => a inicia em 1, e será acrescida até ser igual ou menor que 20.
+//Para verificar se o número é divisível ou não poder 3, uso o operador %, com isso pego somente o resto da divisão do número por 3.
+
+/*Se o resto da divisão de a por 3 for diferente(!=) de zero, imprimo esse valor usando o console.log. 
+Caso seja falso, ou seja, a%3==0, o console.log não é realizado.*/
+
 for(a=1; a<=20; a++){
     if(a%3!=0){
         console.log(a)
