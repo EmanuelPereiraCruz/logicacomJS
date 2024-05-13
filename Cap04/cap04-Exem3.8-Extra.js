85a127e016c68f3377d49458d873a3a59d132093
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-05-13 20:46:56 -0300

    Update cap04-Exem3.8-Extra.js

--- Cap04/cap04-Exem3.8-Extra.js
+++ Cap04/cap04-Exem3.8-Extra.js
@@ -1,14 +1,15 @@
 "use strict"
 /*
 
-CImprimir os números pares de 30 a 10:
+Imprimir os números pares de 30 a 10:
 
-Escreva um programa que use um laço for para imprimir todos os números pares de 30 a 10.
+Escreva um programa que use while para imprimir todos os números pares de 30 a 10.
 */
 
 let count = 30
 
 console.log("|----Números pares entre 30 e 10----|"+"\n")
+
 while(count>=10){
     if(count%2==0){
           console.log(count)
