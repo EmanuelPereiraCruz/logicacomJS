22004aec7d6e4a84a905011b032e13d1f1143703
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-05-19 17:02:06 -0300

    Update cap05-Exem6.3-Extra.js
    Adicionei comentários a respeito da lógica implementada.
    
    - Uso do método slice;
    
    - Uso do método sort;
    
    - Uso do .disabled
    
    Além disso, para resolver o exemplo usei: IF e laço For.

--- Cap05/cap05-Exem6.3-Extra.js
+++ Cap05/cap05-Exem6.3-Extra.js
@@ -1,11 +1,12 @@
-'use strict'
+'use strict' //Uso o modo strict, pois ele me assegura de criar as variáveis.
 
-let listaDeNomes = [];
+let listaDeNomes = []; //Definindo um array global e vazio, será adicionando os elementos posteriormente.
 
 function exibeNomes(){
     let nome = document.getElementById("inNome").value;
     
-    if(nome == ""){
+    if(nome == ""){/*Verificando se o campo nome foi preenchido ou está vazio, 
+    caso o if seja verdadeiro ele exibe uma mensagem através do alert e return para a página.*/
         alert("Por favor, informe um nome.");
         return;
     }
@@ -18,13 +19,13 @@ function exibeNomes(){
 
     if(listaDeNomes.length==5){
 
-        btAdicionar.disabled = true;
-
-        let copiaDeNomes = listaDeNomes.slice();
-        copiaDeNomes.sort();
+        btAdicionar.disabled = true; //Desativei o botão, assim após o usuário informar os 5 elementos do array, ele não poderá inserir mais itens.
+        
+        let copiaDeNomes = listaDeNomes.slice(); //Usando o método slice, eu copio o array originial. Optei por copia-lo, pois não quero alterar nada no array original.
+        copiaDeNomes.sort(); //Usando o método sort, organizo o array em ordem alfabética.
 
         for(let a=0; a<copiaDeNomes.length; a++){
-            listaDeNomesOrdenados+=copiaDeNomes[a]+"\n"
+            listaDeNomesOrdenados+=copiaDeNomes[a]+"\n"//Aqui, concateno a variável com ela mesma e com o item do array copiado.
         }
         
         alert("Lista de Nomes em ordem alfabética: "+"\n\n"+listaDeNomesOrdenados)
