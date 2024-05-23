2475136501c2ad0dfd43fbc41f5567b3f97afa7d
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-05-23 10:53:40 -0300

    Update cap06-Exem2.4.js
    Adicionei observações sobre:
    
    - Construtor new Date( );
    
    - Métodos set para capturar dia, mês e ano de uma variável;
    
    - Operador ternário para formatar saída.

--- Cap06/cap06-Exem2.4.js
+++ Cap06/cap06-Exem2.4.js
@@ -1,24 +1,29 @@
 const DESCONTO = 0.2;
 
 function calculaLimiteData(){
-    let dia = document.getElementById("inDataInfracao").value;
+    let dia = document.getElementById("inDataInfracao").value; //Declarei uma variável denominada dia
     console.log(dia);
 
-    let diaObjeto = new Date(dia)
-    console.log(diaObjeto);
-
-    let valorMulta = document.getElementById("inValorMulta").value;
+    let diaObjeto = new Date(dia) /*Declarei uma segunda variável, nela passei como paramêtro do construtor new Date a variável dia. */
+    console.log(diaObjeto); //Usando o construtor, consigo alterar a data usando os métodos get e set.//
+    
+    let valorMulta = document.getElementById("inValorMulta").value;//Recebi o valor da multa e transformei em um número.
     valorMulta = Number(valorMulta);
 
-    if(dia == "" || valorMulta == 0){
+    if(dia == "" || valorMulta == 0){ //Chequei se os campos foram preenchidos.
         alert("Por favor, prencha os campos corretamente.");
         return;
     }
 
-    let diaHoje = diaObjeto.getDate()+1;
-    let mesHoje = diaObjeto.getMonth()+4;
-    let anoHoje = diaObjeto.getFullYear();
+    let diaHoje = diaObjeto.getDate()+1;/*Usei o método getDate para pegar o dia da variável diaObjeto, 
+    adicionei +1, pois o método retornava o dia anterior, ou seja, dia-1*/
     
+    let mesHoje = diaObjeto.getMonth()+4;/*Usei o método getMonth para pegar o mês da variável diaObjeto, 
+    como esse método retornar o mês anterior, adicionei +4(+1 para o mês atual e +3 para os próximos 3 meses)*/
+    
+    let anoHoje = diaObjeto.getFullYear();/*Aqui, estou pegando o ano da variável.*/
+
+    //Usei operador ternário para melhorar a exibição das informações na saída.
     let diaFormat = diaHoje <10? "0"+diaHoje : diaHoje;
     
     let mesFormat = mesHoje <10? "0"+mesHoje: mesHoje;
