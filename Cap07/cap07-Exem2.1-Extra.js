79b58ed121165e8d168bd1b1573d2de03a52be8d
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-06-02 15:31:14 -0300

    Add files via upload

---
+++ Cap07/cap07-Exem2.1-Extra.js
@@ -0,0 +1,8 @@
+function dobraNumero(n){
+    let numeroDobrado = n*2
+    alert("O dobro de "+numero+" é: "+numeroDobrado)
+}
+
+let numero = Number(prompt("Por favor, informe um número"))
+
+dobraNumero(numero)
