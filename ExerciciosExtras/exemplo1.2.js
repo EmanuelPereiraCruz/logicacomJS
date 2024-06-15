80f229e2c9350cb04956b38166150d33945795e6
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-06-14 21:57:46 -0300

    Update exemplo1.2.js

--- ExerciciosExtras/exemplo1.2.js
+++ ExerciciosExtras/exemplo1.2.js
@@ -1,13 +1,11 @@
 function calculaAreaDoTriangulo(){
     let base_do_triangulo = document.getElementById("inBaseTriangulo").value;
     base_do_triangulo = Number(base_do_triangulo)
-    console.log(base_do_triangulo)
 
     let altura_do_triangulo = document.getElementById("inAlturaTriangulo").value;
     altura_do_triangulo = Number(altura_do_triangulo)
-    console.log(altura_do_triangulo)
 
-
+    //Aqui, faço um IF para verificar se os campos foram preenchidos
     if(base_do_triangulo == "" || isNaN(base_do_triangulo) || altura_do_triangulo == "" || isNaN(altura_do_triangulo)){
         alert("Todos os campos devem ser preenchidos!")
         return;
@@ -15,6 +13,7 @@ function calculaAreaDoTriangulo(){
 
     let area_do_triangulo = (base_do_triangulo*altura_do_triangulo)/2
 
+    //Aqui, para realizar a exibição das informações, usei String Template
     outSaida.textContent = `Base do triângulo: ${base_do_triangulo}cm\n\nAltura do Triângulo: ${altura_do_triangulo}cm\n\nArea do triângulo:${area_do_triangulo}cm²`
 }
 
